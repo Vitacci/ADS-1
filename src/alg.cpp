2964aa11f84e27444eb5a36d997560c46970fac1
Vita <75259415+Vitacci@users.noreply.github.com>
2024-03-02 14:05:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -20,7 +20,7 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
   int cnt = 0;
   int flag = 0;
-  for (int i = 2; i < 100000; ++i) {
+  for (uint64_t i = 2; i < 1000000; ++i) {
     for (int j = 2; j < i; ++j) {
       if (i % j == 0) {
         flag = 1;
