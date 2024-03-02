aaa90acff4fc7c534d0d758cea1671f98bea515e
Vita <75259415+Vitacci@users.noreply.github.com>
2024-03-02 11:42:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,57 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    int flag = 0;
+    for (int i = 2; i < value; i++) {
+        if (value % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+        return false;
+    else
+        return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int cnt = 0;
+    int flag = 0;
+    for (int i = 2; i < 100000; ++i) {
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) {
+            cnt += 1;
+            if (cnt == n)
+                return i;
+        }
+        else {
+            flag = 0;
+        }
+    }
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    int a = value + 1;
+    while (1) {
+        if (checkPrime(a)) {
+            return a;
+        }
+        a++;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int sum = 0;
+    for (int i = 2; i < hbound; ++i) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
