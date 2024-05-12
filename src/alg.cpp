78db64cbf52c21aaedeb73f27af85181d42da017
Vita <75259415+Vitacci@users.noreply.github.com>
2024-05-12 19:57:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,63 +4,52 @@
 
 
 bool checkPrime(uint64_t value) {
-  int flag = 0;
-  for (int i = 2; i < value; i++) {
-    if (value % i == 0) {
-        flag = 1;
-        break;
+  if (value == 1) {
+    return false;
+  } else {
+    for (uint64_t del = 2; del < value; del++) {
+      if (value % del == 0) {
+        return false;
+      }
     }
   }
-  if (flag == 1)
-    return false;
-  else
-    return true;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 1) return 2;
-    uint64_t count = 1;
-    uint64_t num = 3;
-    while (count < n) {
-        bool isPrime = true;
-        for (uint64_t i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            count++;
-            if (count == n) return num;
-        }
-        num += 2;
+  uint64_t count = 0, num = 1;
+  while (count != n) {
+    if (checkPrime(num) == 1) {
+      count++;
+      num++;
+    } else {
+      num++;
     }
-    return 0;
+  }
+  return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int a = value + 1;
-  while (1) {
-    if (checkPrime(a)) {
-      return a;
+  bool chek = false;
+  uint64_t num = value + 1;
+  while (!chek) {
+    if (checkPrime(num) == 1) {
+      chek = true;
     }
-    a++;
+    num++;
   }
+  return num - 1;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
-    for (uint64_t num = 2; num < hbound; ++num) {
-        bool isPrime = true;
-        for (uint64_t i = 2; i <= num / 2; ++i) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            sum += num;
-        }
+  uint64_t sum = 0, num = 1;
+  for (int i = 0; i < hbound; i++) {
+    if (checkPrime(num) == 1) {
+      sum += num;
+      num++;
+    } else {
+      num++;
     }
-    return sum;
+  }
+  return sum;
 }
